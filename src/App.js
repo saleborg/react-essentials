a90c74c9d35ed9e19f76eccf1d68caf84aa6fe35
Stefan Aleborg <stefan.aleborg@gmail.com>
2021-10-28 16:07:42 +0200

    Running essential training

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Routes, Route} from "react-router-dom"
+import {Home, About, Events, Contact, Woops404, Services, Location, CompanyHistory} from "./pages";
 
-function App() {
+
+
+function App({ login }) {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />  
+          <Route path="history" element={<CompanyHistory />} />  
+          <Route path="location" element={<Location />} />  
+
+        </Route>
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Woops404 />} />
+      </Routes>
+      
+
     </div>
   );
+
+
+
+
+
+
 }
 
 export default App;
---
+++ src/functions.test.js
@@ -0,0 +1,5 @@
+import {timesTwo} from "./functions"
+
+test("Multiples by two", () => {
+    expect(timesTwo(4)).toBe(8)
+})
